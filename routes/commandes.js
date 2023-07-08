7bd18379c05cd5fe1791e2dcd65cf6e30bcddea7
francis-tg <franc9885@gmail.com>
2023-07-08 23:46:04 +0000

    add commande

--- routes/commandes.js
+++ routes/commandes.js
@@ -4,16 +4,27 @@ const db = require("../models");
 const router = require("express").Router();
 
 router.get("/", ensureAuthenticated, async (req, res) => {
+	let Commandes = {};
 	const commandes = await db.Commande.findAll({
-		include: db.Article,
 		raw: true,
 	});
 	const articles = await db.Stock.findAll({
 		raw: true,
 		include: db.Article,
 	});
+	commandes.map(async (commande) => {
+		const articles_commandes = await db.Article_commande.findAll({
+			where: { commande_id: commande.id },
+			raw: true,
+		});
+		Commandes = { ...Commandes, ...commande, articles_commandes };
+	});
+	setTimeout(() => {
+		console.log(Commandes);
+	}, 5000);
 	const getUsers = await db.User.findAll({ include: ["Role"], raw: true });
 	const users = getUsers.filter((user) => !user["Role.isAdmin"]);
+
 	return res.render("commandes/", { commandes, articles, users });
 });
 router.get("/live", ensureAuthenticated, async (req, res) => {
@@ -34,7 +45,20 @@ router.post("/add", ensureAuthenticated, (req, res, next) => {
 		.catch((err) => {
 			next(err);
 		}); */
-	console.log(req.body);
+	const { name, numero_client, address_livraison, stock_id, articles } =
+		req.body;
+	db.Commande.create({ name, numero_client, address_livraison }).then(
+		async (commande) => {
+			articles.map(async (article) => {
+				await db.Article_commande.create({
+					article_id: article.article_id,
+					commande_id: commande.id,
+					quantite: article.quantite,
+				});
+			});
+		},
+	);
+	res.status(201).redirect(req.headers.referer);
 });
 
 module.exports = router;

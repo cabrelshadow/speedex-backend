b49971ed78f11bfd70db84782f49afb77e286cd9
cabrel sianou <sianou93@gmail.com>
2023-08-02 23:38:40 +0100

    add route to add ville function

--- routes/pays.js
+++ routes/pays.js
@@ -6,4 +6,14 @@ router.get("/", ensureAuthenticated, async (req, res) => {
 	console.log(req.user);
 	return res.render("pays");
 });
+
+router.get("/", ensureAuthenticated, async (req, res) => {
+	const pays = await db.User.findAll({
+		include: [""],
+		raw: true,
+	});
+	const roles = await db.Role.findAll({ raw: true });
+	res.status(200).render("settings/user", { users, roles });
+});
+
 module.exports = router;
--- routes/ville.js
+++ routes/ville.js
@@ -6,4 +6,24 @@ router.get("/", ensureAuthenticated, async (req, res) => {
 	console.log(req.user);
 	return res.render("ville");
 });
+router.get("/", ensureAuthenticated, async (req, res) => {
+	const villes = await db.Ville.findAll({ raw: true });
+	return res.render("/ville", { villes });
+});
+router.post("/add-ville", async (req, res, next) => {
+	if (Object.keys(req.body).length > 0) {
+		db.Ville.create(req.body)
+			.then(() => {
+				req.session.messages.push({
+					type: "success",
+					msg: "ville a été bien créer",
+				});
+				return res.redirect(req.headers.referer);
+			})
+			.catch((err) => {
+				next(err);
+			});
+	}
+});
+
 module.exports = router;

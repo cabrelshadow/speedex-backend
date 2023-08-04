21daf594c9bbaa775910632be6b53c0c11e3862b
francis-tg <franc9885@gmail.com>
2023-08-04 10:07:37 +0000

    fix bugs

--- routes/ville.js
+++ routes/ville.js
@@ -8,6 +8,15 @@ router.get("/", ensureAuthenticated, async (req, res) => {
 });
 router.post("/add-ville", async (req, res, next) => {
 	if (Object.keys(req.body).length > 0) {
+		const { nomville } = req.body;
+		const getVille = await db.Ville.findOne({ where: { nomville } });
+		if (getVille) {
+			req.session.messages.push({
+				type: "danger",
+				msg: "ville a été déjà créer",
+			});
+			return res.redirect(req.headers.referer);
+		}
 		await db.Ville.create(req.body)
 			.then(() => {
 				req.session.messages.push({
